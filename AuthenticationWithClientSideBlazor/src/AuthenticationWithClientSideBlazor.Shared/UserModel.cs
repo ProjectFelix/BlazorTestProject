69794e7681852fc8a41879d43a16ff4002b78ac2
Project Felix <phat7s@yahoo.com>
2020-02-07 00:19:14 -0500

    Blazor Auth
    Added Blazor Auth Project

---
+++ AuthenticationWithClientSideBlazor/src/AuthenticationWithClientSideBlazor.Shared/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthenticationWithClientSideBlazor.Shared
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+
+        public int GamesPlayed { get; set; }
+        public int GamesWon { get; set; }
+        public int ShotsHit { get; set; }
+        public int ShotsFired { get; set; }
+    }
+}
